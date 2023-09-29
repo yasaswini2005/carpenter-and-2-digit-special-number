7d47980822402646283dd9414a90a63a541f3b22
GURIVI REDDY YASASWINI <gyasu1110@gmail.com>
2023-09-29 23:54:43 +0530

    Update carpenter.c

--- carpenter.c
+++ carpenter.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-   float worker;
+   float worker_fee;
    float t,daily_wage,no_of_workers,left;
    t=4800.00;
    scanf("%f",&daily_wage);
-   worker=daily_wage*4;
+   worker_fee=daily_wage*4;
    scanf("%f",&no_of_workers);
-   left=t-(no_of_workers*worker);
+   left=t-(no_of_workers*worker_fee);
    printf("%f",left);
 
     return 0;
